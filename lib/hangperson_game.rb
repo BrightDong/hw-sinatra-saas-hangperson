aabd2e1746c286e3fdb07a19e4b3eb452e54fe1d
Bright Dong <bright.dong@foxmail.com>
2015-11-28 23:23:01 +0800

    half done

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -10,6 +10,9 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses =''
+    @wrong_guesses =''
+    @word_remain = word
   end
 
   def self.get_random_word
@@ -17,6 +20,29 @@ class HangpersonGame
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
+  end
+
+  attr_accessor :word, :guesses, :wrong_guesses
+
+  def guess(c) 
+      raise ArgumentError, "input is nil or empty" if c == nil || c == ''
+      raise ArgumentError, "input is not a a-zA-Z char" if c[/[a-zA-Z]/] == nil
+      c = c.downcase
+      #repeat guess
+      if @wrong_guesses.index(c) != nil || @guesses.index(c) != nil
+          return false
+      end
+    
+      #correct guess
+      if (@word_remain.delete!(c) != nil)
+          @guesses += c 
+      else
+      #wrong guess
+          if @wrong_guesses.index(c) == nil
+              @wrong_guesses += c
+          end
+      end
+      return true
   end
 
 end
